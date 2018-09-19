0369ce1385b5c735d862e53e7b9eb6d8edaf0c60
nguyen <nguyen@nguyen-Latitude-5580>
2018-09-19 15:31:31 +0200

    Création ActivityDao

---
+++ src/main/java/taa/springboot/service/ActivityDao.java
@@ -0,0 +1,20 @@
+package taa.springboot.service;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import taa.springboot.domain.Activity;
+
+
+@Transactional
+@Component
+public interface ActivityDao  extends JpaRepository <Activity, Long>{
+	
+	@Query("from activity a where a.label = :label")
+	public Activity findByPseudo(@Param("labal") String label);
+	
+	
+}
